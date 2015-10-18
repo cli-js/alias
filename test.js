fe75f3562c8312175ad2ab0ac2d1ef1b8d340b84
Scott Corgan <scottcorgan@gmail.com>
2015-10-17 20:32:04 -0700

    tests

---
+++ test.js
@@ -0,0 +1,17 @@
+var namespace = require('tessed').namespace
+var alias = require('.')
+
+var test = namespace('alias')
+
+test('alias()', function (t) {
+
+  t.deepEqual(alias(), {
+    type: 'alias',
+    value: []
+  }, 'no inputs')
+
+  t.deepEqual(alias('one', 'two'), {
+    type: 'alias',
+    value: ['one', 'two']
+  }, 'multiple inputs')
+})
